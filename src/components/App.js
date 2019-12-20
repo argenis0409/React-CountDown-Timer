4da42db1035583caacead62cb7467367b1ff93cf
Argenis De Los Santos <argenis04_09@icloud.com>
2019-12-19 22:51:00 -0800

    added the timer.js component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Timer from './Timer';
 
 class App extends Component {
   render() {
     return (
       <div className='main-container'>
         <h1>Start Here</h1>
+        <Timer />
       </div>
     )
   }
---
+++ src/components/Timer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Timer extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Timer</h1>
+            </div>
+        )
+    }
+}
