254b7e7081cd72577008e8822c32ed7d65075583
Argenis De Los Santos <argenis04_09@icloud.com>
2019-12-19 23:07:02 -0800

    updated the Timer comopnent

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 
 export default class Timer extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Timer</h1>
-            </div>
-        )
+
+    format(time) {
+        let seconds = time % 60;
+        let minutes = Math.floor(time / 60);
+        
+        minutes = minutes.toString().length === 1 ? "0" + minutes : minutes;
+        seconds = seconds.toString().length === 1 ? "0" + seconds : seconds;
+        return minutes + ':' + seconds;
     }
+
+      render () {
+        const {time} = this.props;
+        return (
+          <div className="displayedTime">
+            <h1>{this.format(time)}</h1>
+          </div>
+        )
+      }
 }
